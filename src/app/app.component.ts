d943facd97ffc6ec6828b009a973d18625bcf15a
aumtuhin <dev.aumtuhin@gmail.com>
2025-02-27 13:45:42 +0300

    feat: initialised project, sett up primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent, HeaderComponent, FooterComponent } from "./components";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/index.ts
@@ -0,0 +1,3 @@
+export {ButtonComponent} from './button/button.component';
+export {HeaderComponent} from './header/header.component';
+export {FooterComponent} from './footer/footer.component';
