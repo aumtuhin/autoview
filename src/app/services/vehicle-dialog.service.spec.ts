1497eecbe4eb539fdd56cfefdbcef6b454392eb7
aumtuhin <dev.aumtuhin@gmail.com>
2025-03-03 09:00:16 +0100

    feat: unit test for dialog service

--- src/app/services/vehicle-dialog.service.spec.ts
+++ src/app/services/vehicle-dialog.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { VehicleDialogService } from './vehicle-dialog.service';
 
 describe('VehicleDialogService', () => {
@@ -12,5 +11,31 @@ describe('VehicleDialogService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should start with dialogState as false', (done) => {
+    service.dialogState$.subscribe((state) => {
+      expect(state).toBeFalse();
+      done(); // Ensures async observable completes
+    });
+  });
+
+  it('should set dialogState to true when openDialog() is called', (done) => {
+    service.openDialog(); // Open dialog
+
+    service.dialogState$.subscribe((state) => {
+      expect(state).toBeTrue();
+      done();
+    });
+  });
+
+  it('should set dialogState to false when closeDialog() is called', (done) => {
+    service.openDialog(); // Open first
+    service.closeDialog(); // Then close
+
+    service.dialogState$.subscribe((state) => {
+      expect(state).toBeFalse();
+      done();
+    });
   });
 });
